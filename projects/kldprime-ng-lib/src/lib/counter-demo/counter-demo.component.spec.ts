faee13308712449b93efbf7bd65e4fa73426da60
Scott Lovy <scott.lovy@krolldiscovery.com>
2021-01-03 08:11:09 -0600

    upgrade to angular 11

--- projects/kldprime-ng-lib/src/lib/counter-demo/counter-demo.component.spec.ts
+++ projects/kldprime-ng-lib/src/lib/counter-demo/counter-demo.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CounterDemoComponent } from './counter-demo.component';
 
@@ -6,7 +6,7 @@ describe('CounterDemoComponent', () => {
   let component: CounterDemoComponent;
   let fixture: ComponentFixture<CounterDemoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CounterDemoComponent ]
     })
--- projects/kldprime-ng-lib/src/lib/kldprime-ng-lib.component.spec.ts
+++ projects/kldprime-ng-lib/src/lib/kldprime-ng-lib.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { KLDPrimeNgLibComponent } from './kldprime-ng-lib.component';
 
@@ -6,7 +6,7 @@ describe('KLDPrimeNgLibComponent', () => {
   let component: KLDPrimeNgLibComponent;
   let fixture: ComponentFixture<KLDPrimeNgLibComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KLDPrimeNgLibComponent ]
     })
